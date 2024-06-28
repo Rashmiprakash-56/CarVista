52c1153eb914b5078b94a7cd4ca59ca6a1aab62c
Rashmiprakash Satapathy <107301318+Rashmiprakash-56@users.noreply.github.com>
2024-06-28 16:28:33 +0530

    Update index.js
    updated the frontend site url

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://carvista-x23d.onrender.com', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
